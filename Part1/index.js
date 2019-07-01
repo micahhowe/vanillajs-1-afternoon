fa8903d0f18ed7255954aa0ec7a1e9cffbec14b5
micahhowe <micahhowe182@gmail.com>
2019-07-01 15:55:47 -0600

    almost done with tic tac

---
+++ Part1/index.js
@@ -0,0 +1,69 @@
+const squares = document.getElementsByTagName('td')
+let board = []
+
+
+//console.log(topRight)
+
+function play(clickedId) {
+    let playerSpan = document.getElementById('player')
+    let clickedElement = document.getElementById(clickedId)
+    if(playerSpan.innerText === 'X' && clickedElement.innerText !== 'X' && clickedElement.innerText !== 'O'){
+        playerSpan.innerText = 'O'
+        clickedElement.innerText = 'X'
+        board[clickedId] = 'X'
+    }
+    else if(playerSpan.innerText === 'O' && clickedElement.innerText !== 'X' && clickedElement.innerText !== 'O') {
+        playerSpan.innerText = 'X'
+        clickedElement.innerText = 'O'
+        board[clickedId] = 'O'
+    }  
+    let topLeft = board[0]
+    let topCenter = board[1]
+    let topRight = board[2]
+    let middleLeft = board[3]
+    let middleCenter = board[4]
+    let middleRight = board[5]
+    let bottomLeft = board[6]
+    let bottomCenter = board[7]
+    let bottomRight = board[8]
+    // Top Match
+    if (topRight !== undefined && topRight === topCenter && topRight === topLeft){
+        console.log(board)
+        
+        setTimeout(alert(topRight + ' is the winner'), 1000)
+    }
+    //Midle Match
+    if (middleRight !== undefined && middleRight === middleCenter && middleRight === middleLeft){
+        console.log(board)
+        setTimeout(alert(middleRight + ' is the winner'), 1000)
+    }
+    //Bottom Match
+    if (bottomRight !== undefined && bottomRight === bottomCenter && bottomRight === bottomLeft){
+        console.log(board)
+        setTimeout(alert(bottomRight + ' is the winner'), 1000)
+    }
+    // All Left Match
+    if (topLeft !== undefined && topLeft === middleLeft && bottomLeft === middleLeft){
+    (alert(topLeft + ' is the winner'), 1000)
+    }
+    //All Center Match
+    if (topCenter !== undefined && middleCenter === bottomCenter && bottomCenter === topCenter){
+        alert(topCenter + ' is the winner')
+        }
+    //All Right Match
+    if (topRight !== undefined && topRight === middleRight && bottomRight === topRight){
+        alert(topRight + ' is the winner')
+        }
+    //console.log(board)
+    if (topRight !== undefined && topRight === middleCenter && middleCenter === bottomLeft){
+        alert(topRight + ' is the winner')
+        }
+}
+function reset(){
+    for(i=0;i<10;i++){
+        document.getElementById(i).innerHTML = ' '    
+    }
+    //console.dir(squares)
+         board = []
+        // console.log(board)
+}
