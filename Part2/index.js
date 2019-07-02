2b8ae8c4718f552712574f695ad4afb1d061da1f
micahhowe <micahhowe182@gmail.com>
2019-07-01 20:21:19 -0600

    The poker color changer is complete

--- Part2/index.js
+++ Part2/index.js
@@ -1,1 +1,17 @@
-console.log('bruh')
+let idInput = document.getElementById('idInput')
+let colorInput = document.getElementById('colorInput')
+//console.log(idInput,colorInput)
+
+function setCard() {
+    let card = document.getElementById(idInput.value)
+    //console.log(2, card)
+    card.style.color = colorInput.value;
+}
+function reset1(){
+    for(b=0;b<5;b++){
+    let cardi = document.getElementsByTagName('section')
+    cardi[b].style.color = "grey"
+    console.log(cardi[b])
+    }
+}
+
